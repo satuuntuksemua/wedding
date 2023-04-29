1a2c2cecddb33e3efe2ed40799c76f4c0ff00c1f
Satu Untuk Semua <satuuntuksemua26@gmail.com>
2023-04-30 04:35:33 +0700

    update

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -1,50 +1,50 @@
-import ImgKembaliBerjumpa from '@assets/images/p-kembali-berjumpa.jpg';
-import ImgJakartaKroasia from '@assets/images/p-jakarta-kroasia.jpg';
-import ImgBoarding from '@assets/images/p-boarding-smg.jpg';
-import ImgKembaliKeIndonesia from '@assets/images/p-kembali-ke-indonesia.jpg';
-import ImgNdodokLawang from '@assets/images/p-ndodok-lawang.jpg';
+import img1 from '@assets/images/p1.jpg';
+import img2 from '@assets/images/p2.jpg';
+import img3 from '@assets/images/p3.jpg';
+import img4 from '@assets/images/p4.jpg';
+import img5 from '@assets/images/p5.jpg';
 
 export const stories = [
   {
     title: 'Jumpa Pertama ',
-    date: '10 Oktober 2015',
+    date: '17 September 2019',
     description:
       'Pertama kali kami dipertemukan di sebuah acara malam keakraban di kampus Universitas Dian Nuswantoro. Sebagai seorang mahasiswa baru, kala itu Latif melihat Wiwil sedang sibuk memegang megaphone (toa) di tengah hiruk pikuk acara. Belum ada sesuatu yang spesial, kala itu justru Latif melihat Wiwil banyak menerima surat pink (tanda kagum) dari teman sesama mahasiswa barunya.',
     image: '',
   },
   {
     title: 'Kembali Berjumpa',
-    date: '9 September 2017',
+    date: '30 September 2019',
     description:
       'Konferensi Future Leader Summit 2017 kembali mempertemukan mereka. Latif sebagai peserta dan Wiwil sebagai panitia acara. Mulai dari saling sapa, mereka berdua tumbuh perasaan saling ingin mengenal lebih dalam. Seusai acara, Wiwil memberanikan diri untuk membangun komunikasi dengan Latif. Dari pesan singkat pertama yang dikirimnya, Wiwil dan Latif mulai dekat dan menjadi lebih sering mengirim kabar. ',
-    image: ImgKembaliBerjumpa,
+    image: img1,
   },
   {
     title: 'Kisah Kasih di Kampus',
-    date: 'Oktober 2017 - 2018',
+    date: 'Oktober 2019 - 2021',
     description:
       'Sebagai sepasang mahasiswa dan mahasiswi, Latif dan Wiwil memilih untuk mengisi kisah kasih di kampusnya dengan mengikuti berbagai kompetisi bersama. Kala itu, Latif adalah mahasiswi Ilmu Komunikasi dan Wiwil adalah mahasiswa Teknik Informatika. Merupakan kolaborasi yang pas jika mengikuti kompetisi teknologi, Wiwil berperan sebagai seorang programmer dan Latif sebagai seorang content development. Latif dan Wiwil pernah membuat aplikasi Ayo Toba (Aplikasi Pariwisata Danau Toba) yang diajukan dalam kompetisi IT FEST Universitas Sumatera Utara di awal tahun 2018.',
-    image: ImgJakartaKroasia,
+    image: img2,
   },
   {
     title: 'Jakarta & Kroasia',
-    date: '2018 - 2019',
+    date: '2021 - 2022',
     description:
       'Tidak selalu bersama, perjuangan dimulai ketika Wiwil memutuskan untuk bekerja di Jakarta sebagai seorang Software Engineer di Tokopedia. Belum usai masa LDR, pada 3 Oktober 2018, Latif juga memutuskan untuk mengambil beasiswa di University of Split, Kroasia. Di bulan November 2018, Wiwil melangsungkan wisuda dan Latif hanya bisa memberi virtual support melalui video call. ',
-    image: ImgBoarding,
+    image: img3,
   },
   {
     title: 'Kembali ke Indonesia',
-    date: '3 Maret 2019',
+    date: '3 Maret 2023',
     description:
       'Periode belajar di Kroasia telah selesai. Akhirnya Latif kembali ke Indonesia! Ada tanggung jawab yang harus Latif selesaikan seusai dari Kroasia, yakni harus menyelesaikan skripsi dan mencapai target untuk wisuda di bulan Agustus 2019. Selain itu, kembalinya Latif ke Indonesia menjadi momen yang tepat bagi Wiwil untuk menyampaikan niat ingin serius kepada orang tua Latif. Hal itu disambut baik oleh kedua orang tua mereka  dan sepakat akan melaksanakan acara Ndodok Lawang usai Latif wisuda. Sebelum wisuda, Latif sudah harus meniti karir di Jakarta sebagai seorang Content Analyst di Ruangguru.',
-    image: ImgKembaliKeIndonesia,
+    image: img4,
   },
   {
     title: 'Ndodok Lawang',
-    date: '31 Agustus 2019',
+    date: '13 Mei 2023',
     description:
       'Akhirnya tiba di masa Wiwil dan keluarga hadir ke rumah Latif untuk menyampaikan niat baik. Mulai dari acara Ndodok Lawang ini, dilanjutkan dengan persiapan pernikahan yang insha Allah akan diselenggarakan pada tanggal 3 Oktober 2020.',
-    image: ImgNdodokLawang,
+    image: img5,
   },
 ];
