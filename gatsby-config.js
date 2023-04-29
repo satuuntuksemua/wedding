1920981a53f1b64724a44d312537b619792532a9
Satu Untuk Semua <satuuntuksemua26@gmail.com>
2023-04-29 21:26:23 +0700

    update tanggal

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  pathPrefix: `/thekusuma`,
+  pathPrefix: `/`,
   plugins: [
     `gatsby-plugin-react-helmet`,
     'gatsby-plugin-eslint',
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,12 @@
 /**
  * Link
  */
-export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20201003T040000Z%2F20201003T060000Z&text=Dinda+%26+Indra+Wedding&details=Dinda+%26+Indra+Wedding`;
-export const GOOGLE_MAPS_LINK = `https://g.page/HARRISHotelSentralandSemarang?share`;
+export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20230708T040000Z/20230708T060000Z&text=Latif+%26+Wiwil+Wedding&details=Latif+%26+Wiwil+Wedding`;
+export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/Gy8y9FD32N1SyJto9`;
 
 /**
  * Wedding time
  */
 export const EPOCH_START_EVENT = 1613952139;
 export const EPOCH_END_EVENT = 1613952139;
-export const UTC_WEDDING_TIME = '2020-10-03:04:00:00Z';
+export const UTC_WEDDING_TIME = '2023-07-08:04:00:00Z';
