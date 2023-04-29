25df7577c48c52db1030f12b32307aaca091d373
Satu Untuk Semua <satuuntuksemua26@gmail.com>
2023-04-29 21:38:02 +0700

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,6 +7,6 @@ export const GOOGLE_MAPS_LINK = `https://goo.gl/maps/Gy8y9FD32N1SyJto9`;
 /**
  * Wedding time
  */
-export const EPOCH_START_EVENT = 1613952139;
-export const EPOCH_END_EVENT = 1613952139;
+export const EPOCH_START_EVENT = 1688895000;
+export const EPOCH_END_EVENT = 1688916600;
 export const UTC_WEDDING_TIME = '2023-07-08:04:00:00Z';
