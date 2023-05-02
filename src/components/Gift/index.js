7bc6bec090bc156b8813f485bbad91fa81974f63
Satu Untuk Semua <satuuntuksemua26@gmail.com>
2023-05-02 16:17:55 +0700

    update pembayaran

--- src/components/Gift/index.js
+++ src/components/Gift/index.js
@@ -38,7 +38,7 @@ function GiftSection() {
         </div>
         {/* BOX INFO */}
         <div className="row">
-          <div className="col-md-12" style={{border}}>
+          <div className="col-md-12" style={{}}>
             <SectionBox icon={IconBCA} text="Bank Central Asia (BCA) : " />
             <span style={{fontWeight: 500, fontSize: '1.2em'}}>Siti Khotimatul Wildah: <b>0380043882 </b> <CopyButton text="0380043882" /></span>
             <SectionBox icon={IconPermata} text="Bank Permata : " /> 
@@ -48,7 +48,7 @@ function GiftSection() {
           </div>
         </div>
         <div className="row">
-          <div className="col-md-8 col-md-offset-2 fh5co-heading" style={{border}}>
+          <div className="col-md-8 col-md-offset-2 fh5co-heading" style={{}}>
             <h2>
               Jika teman-teman ingin mengirimkan kado ke Alamat di bawah ini:
             </h2>
