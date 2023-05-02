30e0ad5b9793fc2199e4b86d961cc13d8edf4dff
Satu Untuk Semua <satuuntuksemua26@gmail.com>
2023-05-02 11:39:14 +0700

    update

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -1,50 +1,51 @@
-import img1 from '@assets/images/p1.jpg';
-import img2 from '@assets/images/p2.jpg';
+import img1 from '@assets/images/p1.jpeg';
+import img2 from '@assets/images/p2.jpeg';
 import img3 from '@assets/images/p3.jpg';
 import img4 from '@assets/images/p4.jpg';
 import img5 from '@assets/images/p5.jpg';
+import img6 from '@assets/images/p6.jpeg';
 
 export const stories = [
   {
     title: 'Jumpa Pertama ',
-    date: '17 September 2019',
+    date: '13 September 2019',
     description:
-      'Pertama kali kami dipertemukan di sebuah acara malam keakraban di kampus Universitas Dian Nuswantoro. Sebagai seorang mahasiswa baru, kala itu Wiwil melihat Latifsedang sibuk memegang megaphone (toa) di tengah hiruk pikuk acara. Belum ada sesuatu yang spesial, kala itu justru Wiwil melihat Latifbanyak menerima surat pink (tanda kagum) dari teman sesama mahasiswa barunya.',
-    image: '',
+      'Pertama kali keduanya dipertemukan di sebuah rangkaian acara penerimaan mahasiswa baru pasca sarjana yang disebut Martikulasi di kampus Universitas Nusa Mandiri. Keduanya tidak begitu akrab hanya saling tahu nama satu sama lain saja.',
+    image: img6,
   },
   {
-    title: 'Kembali Berjumpa',
-    date: '30 September 2019',
+    title: 'Perkuliahan Dimulai',
+    date: '20 September 2019',
     description:
-      'Konferensi Future Leader Summit 2017 kembali mempertemukan mereka. Wiwil sebagai peserta dan Latifsebagai panitia acara. Mulai dari saling sapa, mereka berdua tumbuh perasaan saling ingin mengenal lebih dalam. Seusai acara, Latifmemberanikan diri untuk membangun komunikasi dengan Wiwil. Dari pesan singkat pertama yang dikirimnya, Latifdan Wiwil mulai dekat dan menjadi lebih sering mengirim kabar. ',
+      'Keduanya menjalani perkuliahan sebagaimana mestinya, tidak begitu akrab namun cukup sesekali menyapa. Wiwil yang cukup membaur dengan yang lain akan tetapi tidak terlalu memperhatikan sekitar jika diluar circlenya (Ary dan Ali) hihi. Latif yang cukup pendiam dan menyendiri. Si kurang peka dan si dingin tak pernah terbayang akan bersanding hihi. Sayang baru 2 semester mengenyam perkuliahan offline pandemi menyerang keduanya masih tidak saling memiliki rasa. Wiwil dengan kisah cintanya didekati beberapa pria dan latif pun dengan kisah cintanya.',
     image: img1,
   },
   {
-    title: 'Kisah Kasih di Kampus',
-    date: 'Oktober 2019 - 2021',
+    title: 'Mulai dekat dan jalan pertama',
+    date: '2 Oktober 2021',
     description:
-      'Sebagai sepasang mahasiswa dan mahasiswi, Wiwil dan Latifmemilih untuk mengisi kisah kasih di kampusnya dengan mengikuti berbagai kompetisi bersama. Kala itu, Wiwil adalah mahasiswi Ilmu Komunikasi dan Latifadalah mahasiswa Teknik Informatika. Merupakan kolaborasi yang pas jika mengikuti kompetisi teknologi, Latifberperan sebagai seorang programmer dan Wiwil sebagai seorang content development. Wiwil dan Latifpernah membuat aplikasi Ayo Toba (Aplikasi Pariwisata Danau Toba) yang diajukan dalam kompetisi IT FEST Universitas Sumatera Utara di awal tahun 2018.',
+      'Awal mula keduanya akrab, saat itu wiwil dengan statusnya yang benar² sendiri begitu pun latif hihi. Katanya wiwil sudah taubat dan tidak ingin menggantung perasaan para lelaki, perkataan rifqi cukup di dengarkan rupanya (rifqi teman sekelas juga). Wiwil mengeluh dan iri dengan kaum pria karena bisa pergi kemanapun tanpa khawatir, dan disana latif berinisiatif untuk menawarkan bagaimana kalau kita pergi ketempat yang belum pernah dituju wiwil sebelumnya. Wiwil mengiyakan dan keduanya pun pergi ke Hutan Mangroove Jakarta Utara. ',
     image: img2,
   },
   {
-    title: 'Jakarta & Kroasia',
-    date: '2021 - 2022',
+    title: 'Wisuda bareng ',
+    date: '20 November 2021 ',
     description:
-      'Tidak selalu bersama, perjuangan dimulai ketika Latifmemutuskan untuk bekerja di Jakarta sebagai seorang Software Engineer di Tokopedia. Belum usai masa LDR, pada 3 Oktober 2018, Wiwil juga memutuskan untuk mengambil beasiswa di University of Split, Kroasia. Di bulan November 2018, Latifmelangsungkan wisuda dan Wiwil hanya bisa memberi virtual support melalui video call. ',
+      'Lulus dan wisuda bareng di 2021, lulus setelah kuliah dijaman pandemi. Wiwil pernah bilang kalau ia merasa sedikit tersentuh dengan perlakuan latif kala itu di hari H wisuda. Wiwil yang kala itu di wisuda akan tetapi ia yang pada periode itu menjadi panitia wisuda tetap ingin membantu dahulu tim registrasi untuk checkin wisudawan, alhasil ia menjadi wisudawan yang hampir terakhir masuk ke aula prosesi dan kalian tahu, latif menunggu di pojok tempat registrasi sehingga saat itu kursi wisudawan kedua nya menjadi berdampingan. Oh ya dan mereka pun melakukan poto bersama (berdua) setelah selesai acara wisuda. Sebagai catatan kedua nya dekat akan tetapi sepertinya yang lain tidak terlalu menyadari itu. Mulai dari sana keduanya semakin akrab, sering bertukar pesan, vicall maupun pergi keluar.',
     image: img3,
   },
   {
-    title: 'Kembali ke Indonesia',
-    date: '3 Maret 2023',
+    title: 'Tanda serius',
+    date: '17 September 2022',
     description:
-      'Periode belajar di Kroasia telah selesai. Akhirnya Wiwil kembali ke Indonesia! Ada tanggung jawab yang harus Wiwil selesaikan seusai dari Kroasia, yakni harus menyelesaikan skripsi dan mencapai target untuk wisuda di bulan Agustus 2019. Selain itu, kembalinya Wiwil ke Indonesia menjadi momen yang tepat bagi Latifuntuk menyampaikan niat ingin serius kepada orang tua Wiwil. Hal itu disambut baik oleh kedua orang tua mereka  dan sepakat akan melaksanakan acara Ndodok Lawang usai Wiwil wisuda. Sebelum wisuda, Wiwil sudah harus meniti karir di Jakarta sebagai seorang Content Analyst di Ruangguru.',
+      'Seiring berjalannya waktu keduanya menjadi semakin akrab dan di awal April ketika ramadhan, tepatnya di tanggal 10 April 2022 Latif mendeklarasikan dan menegaskan mengenai hubungan keduanya. Hubungan keduanya semakin serius, dan Wiwil pun makin akrab dengan keluarga Latif, ia akrab dengan ayahnya latif dan juga bibi²nya hal tersebut sangat menyenangkan untuk Wiwil. Terlebih ia seperti mendapatkan kembali sosok ayah. Singkat cerita di bulan September yaitu kelahiran Wiwil, Latif datang ke Sukabumi ingin berjumpa dan mengobrol dengan keluarga (Kaka Wiwil) dan memberitahukan kalau ia serius dengan memberikan kado cincin.',
     image: img4,
   },
   {
-    title: 'Ndodok Lawang',
+    title: 'Lamaran',
     date: '13 Mei 2023',
     description:
-      'Akhirnya tiba di masa Latifdan keluarga hadir ke rumah Wiwil untuk menyampaikan niat baik. Mulai dari acara Ndodok Lawang ini, dilanjutkan dengan persiapan pernikahan yang insha Allah akan diselenggarakan pada tanggal 3 Oktober 2020.',
+      'Akhirnya tiba di masa Latif dan keluarga hadir ke rumah Wiwil untuk menyampaikan niat baik. Mulai dari acara lamaran ini, dilanjutkan dengan persiapan pernikahan yang insha Allah akan diselenggarakan pada tanggal 9 Juli 2023.',
     image: img5,
   },
 ];
