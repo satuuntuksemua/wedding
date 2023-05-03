7a1f8595ad909378c488b745fa8dc5ab044ba823
Satu Untuk Semua <satuuntuksemua26@gmail.com>
2023-05-03 23:17:34 +0700

    up thm

--- src/components/PhotoSection/photo-data.js
+++ src/components/PhotoSection/photo-data.js
@@ -8,10 +8,6 @@ import Gallery4 from '@assets/images/gallery-4.jpg';
 import GalleryT4 from '@assets/images/gallery-t-4.jpg';
 import Gallery5 from '@assets/images/gallery-5.jpg';
 import GalleryT5 from '@assets/images/gallery-t-5.jpg';
-import Image5 from '@assets/images/slide-5.jpg';
-import Image5thumb from '@assets/images/slide-5-thumb.jpg';
-import Image6 from '@assets/images/slide-6.jpg';
-import Image6thumb from '@assets/images/slide-6-thumb.jpg';
 import Gallery6 from '@assets/images/gallery-6.jpg';
 import GalleryT6 from '@assets/images/gallery-t-6.jpg';
 import Gallery7 from '@assets/images/gallery-7.jpg';
@@ -45,14 +41,6 @@ export const photos = [
   {
     original: Gallery3,
     thumbnail: GalleryT3,
-  },
-  {
-    original: Image6,
-    thumbnail: Image6thumb,
-  },
-  {
-    original: Image5,
-    thumbnail: Image5thumb,
   },
   {
     original: Gallery4,
