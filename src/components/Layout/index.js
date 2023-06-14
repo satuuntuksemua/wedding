92b138ca7e0ae8a7628d6ee7cf3998578022d503
Satu Untuk Semua <63195112+satuuntuksemua@users.noreply.github.com>
2023-06-14 17:09:05 +0700

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -7,7 +7,7 @@ import '@assets/css/icomoon.css';
 import '@assets/css/bootstrap.css';
 import '@assets/css/style.css';
 
-const IMAGE_URL = `https://latiefdole.my.id/static/slide-6-4715e29302dbaa2ba21494c6258298d4.jpg`;
+const IMAGE_URL = `https://latiefdole.my.id/static/bg-wedding-b60f5067f0791ad623ddd7279700a6a1.jpg`;
 const META_DESCRIPTION = `Dengan memohon Rahmat dan Ridho Illahi, teriring niat menjalankan Sunnah Rasulullah ﷺ untuk membentuk rumah tangga yang Sakinah, Mawaddah wa Rahmah, kami mohon do'a agar senantiasa diberikan kelancaran dan keberkahan. - Wiwil & Latif`;
 
 function MainLayout({ children }) {
@@ -20,12 +20,12 @@ function MainLayout({ children }) {
         <link rel="icon" type="image/png" href={Favicon} />
 
         {/* font and SEO tags */}
-        <meta property="og:title" content="The Wedding of Wiwil & Wiwil" />
+        <meta property="og:title" content="The Wedding of Wiwil & Latif" />
         <meta property="og:image" content={IMAGE_URL} />
         <meta property="og:url" content="https://latiefdole.my.id" />
-        <meta property="og:site_name" content="The Wedding of Wiwil & Wiwil" />
+        <meta property="og:site_name" content="The Wedding of Wiwil & Latif" />
         <meta property="og:description" content={META_DESCRIPTION} />
-        <meta name="twitter:title" content="The Wedding of Wiwil & Wiwil" />
+        <meta name="twitter:title" content="The Wedding of Wiwil & Latif" />
         <meta name="twitter:description" content={META_DESCRIPTION} />
         <meta name="twitter:image" content={IMAGE_URL} />
         <meta name="twitter:url" content="https://latiefdole.my.id" />
