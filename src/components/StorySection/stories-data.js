6d116bd9716204980a99e5bb0e1c0cd698e2d606
Satu Untuk Semua <63195112+satuuntuksemua@users.noreply.github.com>
2023-07-01 23:03:22 +0700

    Update stories-data.js

--- src/components/StorySection/stories-data.js
+++ src/components/StorySection/stories-data.js
@@ -17,28 +17,28 @@ export const stories = [
     title: 'Perkuliahan Dimulai',
     date: '20 September 2019',
     description:
-      'Keduanya menjalani perkuliahan sebagaimana mestinya, tidak begitu akrab namun cukup sesekali menyapa. Wiwil yang cukup membaur dengan yang lain akan tetapi tidak terlalu memperhatikan sekitar jika diluar circlenya (Ary dan Ali) hihi. Latif yang cukup pendiam dan menyendiri. Si kurang peka dan si dingin tak pernah terbayang akan bersanding hihi. Sayang baru 2 semester mengenyam perkuliahan offline pandemi menyerang keduanya masih tidak saling memiliki rasa. Wiwil dengan kisah cintanya didekati beberapa pria dan latif pun dengan kisah cintanya.',
+      'Keduanya menjalani perkuliahan sebagaimana mestinya, tidak begitu akrab namun cukup sesekali menyapa. Wiwil yang cukup membaur dengan yang lain akan tetapi tidak terlalu memperhatikan sekitar jika diluar circlenya (Ary dan Ali) hihi. Latif yang cukup pendiam dan menyendiri.  Sayang baru 2 semester mengenyam perkuliahan offline pandemi menyerang keduanya masih tidak saling memiliki rasa. ',
     image: img1,
   },
   {
     title: 'Mulai Dekat dan Jalan Pertama',
     date: '2 Oktober 2021',
     description:
-      'Awal mula keduanya akrab, saat itu wiwil dengan statusnya yang benar² sendiri begitu pun latif hihi. Katanya wiwil sudah taubat dan tidak ingin menggantung perasaan para lelaki, perkataan rifqi cukup di dengarkan rupanya (rifqi teman sekelas juga). Wiwil mengeluh dan iri dengan kaum pria karena bisa pergi kemanapun tanpa khawatir, dan disana latif berinisiatif untuk menawarkan bagaimana kalau kita pergi ketempat yang belum pernah dituju wiwil sebelumnya. Wiwil mengiyakan dan keduanya pun pergi ke Hutan Mangroove Jakarta Utara. ',
+      'Awal mula keduanya akrab, saat itu wiwil dengan statusnya yang benar² sendiri begitu pun latif. Katanya wiwil sudah taubat dan tidak ingin menggantung perasaan laki-laki jika memang tidak pernah ada niat serius, perkataan rifqi cukup di dengarkan rupanya (rifqi teman sekelas juga). Wiwil mengeluh dan iri dengan kaum pria karena bisa pergi kemanapun tanpa khawatir, dan disana latif berinisiatif untuk menawarkan bagaimana kalau kita pergi ketempat yang belum pernah dituju wiwil sebelumnya. Wiwil mengiyakan dan keduanya pun pergi ke Hutan Mangroove Jakarta Utara. ',
     image: img2,
   },
   {
     title: 'Wisuda Bareng ',
     date: '20 November 2021 ',
     description:
-      'Lulus dan wisuda bareng di 2021, lulus setelah kuliah dijaman pandemi. Wiwil pernah bilang kalau ia merasa sedikit tersentuh dengan perlakuan latif kala itu di hari H wisuda. Wiwil yang kala itu di wisuda akan tetapi ia yang pada periode itu menjadi panitia wisuda tetap ingin membantu dahulu tim registrasi untuk checkin wisudawan, alhasil ia menjadi wisudawan yang hampir terakhir masuk ke aula prosesi dan kalian tahu, latif menunggu di pojok tempat registrasi sehingga saat itu kursi wisudawan kedua nya menjadi berdampingan. Oh ya dan mereka pun melakukan poto bersama (berdua) setelah selesai acara wisuda. Sebagai catatan kedua nya dekat akan tetapi sepertinya yang lain tidak terlalu menyadari itu. Mulai dari sana keduanya semakin akrab, sering bertukar pesan, vicall maupun pergi keluar.',
+      'Lulus dan wisuda bareng di 2021, lulus setelah kuliah dijaman pandemi. Wiwil pernah bilang kalau ia merasa sedikit tersentuh dengan perlakuan latif kala itu di hari H wisuda. Wiwil yang kala itu di wisuda akan tetapi ia yang pada periode itu menjadi panitia wisuda tetap ingin membantu dahulu tim registrasi untuk check-in wisudawan, alhasil ia menjadi wisudawan yang hampir terakhir masuk ke aula prosesi dan kalian tahu, latif menunggu di pojok tempat registrasi sehingga saat itu kursi wisudawan kedua nya menjadi berdampingan. Oh ya dan mereka pun melakukan poto bersama (berdua) setelah selesai acara wisuda.',
     image: img3,
   },
   {
     title: 'Tanda Serius',
     date: '17 September 2022',
     description:
-      'Seiring berjalannya waktu keduanya menjadi semakin akrab dan di awal April ketika ramadhan, tepatnya di tanggal 10 April 2022 Latif mendeklarasikan dan menegaskan mengenai hubungan keduanya. Hubungan keduanya semakin serius, dan Wiwil pun makin akrab dengan keluarga Latif, ia akrab dengan ayahnya latif dan juga bibi²nya hal tersebut sangat menyenangkan untuk Wiwil. Terlebih ia seperti mendapatkan kembali sosok ayah. Hubungan keduanya tentunya tidak selalu berjalan mulus namun karena itulah mereka bisa saling menguatkan. Terkadang mereka butuh penasehat juga seperti Ary dan Rangga (Teman Keduanya). Singkat cerita di bulan September yaitu kelahiran Wiwil, Latif datang ke Sukabumi ingin berjumpa dan mengobrol dengan keluarga (Kaka Wiwil) dan memberitahukan kalau ia serius dengan memberikan kado cincin . ',
+      'Seiring berjalannya waktu keduanya menjadi semakin akrab, hubungan keduanya serius, dan Wiwil pun akrab dengan keluarga Latif. Tentunya tidak selalu berjalan mulus namun karena itulah mereka bisa saling menguatkan. Terkadang mereka butuh penasehat juga seperti Ary dan Rangga (Teman Keduanya). Singkat cerita di bulan September yaitu kelahiran Wiwil, Latif datang ke Sukabumi ingin berjumpa dan mengobrol dengan keluarga (Kaka Wiwil) dan memberitahukan kalau ia serius.',
     image: img4,
   },
   {
